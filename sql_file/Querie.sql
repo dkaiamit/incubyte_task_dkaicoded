8ca64677a0ce60f9b4e4c7c3a756a6a65ab31f8d
dkai <dkai@kali>
2025-02-07 15:46:28 +0530

    Updated queries.sql file

--- sql_file/Querie.sql
+++ sql_file/Querie.sql
@@ -17,3 +17,53 @@ FROM `test-74ac1.sales_data.sales_table`
 GROUP BY ProductName 
 ORDER BY Total_Sales DESC 
 LIMIT 6;
+
+##Sale Distribution by Store Type
+SELECT 
+    StoreType, 
+    SUM(TransactionAmount) AS Total_Sales, 
+    COUNT(TransactionID) AS Total_Transactions 
+FROM `test-74ac1.sales_data.sales_table` 
+GROUP BY StoreType;
+
+DRILL DOWN METRICS
+##Region wise revenue breakdown
+SELECT 
+    Region, 
+    SUM(TransactionAmount) AS Total_Sales, 
+    COUNT(TransactionID) AS Transactions 
+FROM `test-74ac1.sales_data.sales_table`
+GROUP BY Region 
+ORDER BY Total_Sales DESC;
+
+
+###Impact of Discounts
+SELECT 
+    CASE 
+        WHEN DiscountPercent > 0 THEN 'Discounted Sales'
+        ELSE 'Full Price Sales'
+    END AS Sale_Type,
+    COUNT(TransactionID) AS Transactions,
+    SUM(TransactionAmount) AS Total_Sales 
+FROM `test-74ac1.sales_data.sales_table` 
+GROUP BY Sale_Type;
+
+###Preferred Payment Mode
+
+SELECT 
+    PaymentMethod, 
+    COUNT(TransactionID) AS Transactions, 
+    SUM(TransactionAmount) AS Total_Sales 
+FROM `test-74ac1.sales_data.sales_table` 
+GROUP BY PaymentMethod 
+ORDER BY Total_Sales DESC;
+
+###Returnby Product
+SELECT 
+    ProductName, 
+    COUNT(TransactionID) AS Total_Returns 
+FROM `test-74ac1.sales_data.sales_table` 
+WHERE Returned = 'Yes'
+GROUP BY ProductName 
+ORDER BY Total_Returns DESC 
+LIMIT 5;
