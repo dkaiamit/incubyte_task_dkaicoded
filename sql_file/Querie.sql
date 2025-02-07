2e8080b167053ff692a55e736a4bb92639009418
dkai <dkai@kali>
2025-02-07 15:44:24 +0530

    Updated queries.sql file

--- sql_file/Querie.sql
+++ sql_file/Querie.sql
@@ -3,3 +3,8 @@ SELECT
     COUNT(DISTINCT TransactionID) AS Total_Transactions, 
     SUM(TransactionAmount) AS Total_Revenue 
 FROM `test-74ac1.sales_data.sales_table`;
+
+##Average Order Value
+SELECT 
+    SUM(TransactionAmount) / COUNT(DISTINCT TransactionID) AS Average_Order_Value 
+FROM  `test-74ac1.sales_data.sales_table`;
