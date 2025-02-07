feedba5dfed13a0b388b97977670a010431c6096
dkai <dkai@kali>
2025-02-07 15:45:21 +0530

    Updated queries.sql file

--- sql_file/Querie.sql
+++ sql_file/Querie.sql
@@ -8,3 +8,12 @@ FROM `test-74ac1.sales_data.sales_table`;
 SELECT 
     SUM(TransactionAmount) / COUNT(DISTINCT TransactionID) AS Average_Order_Value 
 FROM  `test-74ac1.sales_data.sales_table`;
+
+##Top 5 Products by Revenue
+SELECT 
+    ProductName, 
+    SUM(TransactionAmount) AS Total_Sales 
+FROM `test-74ac1.sales_data.sales_table`
+GROUP BY ProductName 
+ORDER BY Total_Sales DESC 
+LIMIT 6;
